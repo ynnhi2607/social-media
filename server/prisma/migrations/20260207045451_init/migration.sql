09fc5f5fc31537cfabde6df0193218b740619b1e
Nguyễn Duy Nhất <nguyenduynhat.250505@gmail.com>
2026-02-07 11:56:30 +0700

    feat: update Prisma schema and add user migration with new fields

---
+++ server/prisma/migrations/20260207045451_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'user',
+    "full_name" TEXT,
+    "bio" TEXT,
+    "status" TEXT NOT NULL DEFAULT 'active',
+    "remember_token" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");

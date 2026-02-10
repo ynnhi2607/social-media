14efd4d52ccc86e1d2b6caf261e27b5932febf08
Nguyễn Duy Nhất <nguyenduynhat.250505@gmail.com>
2026-02-11 00:06:49 +0700

    feat: integrate PostgreSQL adapter for Prisma and update server configuration

--- server/src/services/post.service.ts
+++ server/src/services/post.service.ts
@@ -1,5 +1,10 @@
 import { PrismaClient } from "../generated/prisma/client.js";
-const prisma = new PrismaClient();
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 export async function createPost(
   authorId: string,
